7b2352c688b825f3f11c5661d753c18e500f6ff8
cagenagnon <enagnon.ago@epitech.eu>
2025-10-17 12:24:41 +0100

    Add route for lists links

--- shortener_url/app/Http/Controllers/ShortUrlController.php
+++ shortener_url/app/Http/Controllers/ShortUrlController.php
@@ -8,6 +8,12 @@ use Illuminate\Support\Str;
 
 class ShortUrlController extends Controller
 {
+
+    public function index()
+    {
+        $links = ShortUrl::all();
+        return $links;
+    }
     public function shorten(Request $request)
     {
         $request->validate([
@@ -31,12 +37,14 @@ class ShortUrlController extends Controller
             'id' => $request->id,
             'original_url' => $request->url,
             'code' => $custom_code,
-            'created_at' => $request->date_timestamp 
+            'created_at' => $request->date_timestamp
 
         ]);
 
         return response()->json(
-            $shortUrl, 201);
+            $shortUrl,
+            201
+        );
     }
 
     public function redirect($code)
@@ -44,6 +52,8 @@ class ShortUrlController extends Controller
         $shortUrl = ShortUrl::where('code', $code)->firstOrFail();
         return redirect($shortUrl->original_url);
     }
+
+    public function clickcount($code) {
+
+    }
 }
-
-
--- shortener_url/routes/api.php
+++ shortener_url/routes/api.php
@@ -10,3 +10,5 @@ Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
 });
 
 Route::post('/shorten', [ShortUrlController::class, 'shorten']);
+Route::get('/links', [ShortUrlController::class, 'index']);
+Route::get('/s/{code}', [ShortUrlController::class, 'index']);
